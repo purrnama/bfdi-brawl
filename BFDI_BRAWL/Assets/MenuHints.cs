934324f2cd6caa7bf49e73004665d78714e7d6a4
Qayyum Yazid <smolpadok@protonmail.com>
2022-06-26 00:17:13 +0200

    More menu interface, and begin settings menu

--- BFDI_BRAWL/Assets/MenuHints.cs
+++ BFDI_BRAWL/Assets/MenuHints.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuHints : MonoBehaviour
 {
+    
+    TextMeshProUGUI text;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<TextMeshProUGUI>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void HoverHint(string hint){
+        text.text = hint;
+        anim.SetTrigger("HintPrompt");
     }
 }
